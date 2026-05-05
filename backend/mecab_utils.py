268de2ccf76b0e865d3794ef1932d0798240cae5
ohio0925 <sinteito@gmail.com>
2026-05-05 21:57:55 +0900

    mecabをfugashiに修正

--- backend/mecab_utils.py
+++ backend/mecab_utils.py
@@ -1,28 +1,23 @@
-import MeCab
+from fugashi import Tagger
 import re
 
-# MeCab初期化（1回だけ）
-tagger = MeCab.Tagger(
-    r'-r "C:\Program Files\MeCab\etc\mecabrc" -d "C:\Program Files\MeCab\dic\ipadic"'
-)
-tagger.parse("")
+# 初期化（パス指定いらない）
+tagger = Tagger()
 
 def mecab_sep(text):
-    node = tagger.parseToNode(str(text))
-
     words_list = []
 
-    while node:
-        features = node.feature.split(",")
+    for token in tagger(str(text)):
+        pos = token.feature.pos1
 
-        if len(features) > 7:
-            pos = features[0]
-            base = features[6] if features[6] != "*" else node.surface
+        if pos in ["名詞", "形容詞", "動詞"]:
+            if re.search("[ァ-ン一-龥]", token.surface):
 
-            if pos in ["名詞", "形容詞", "動詞"]:
-                if re.search("[ァ-ン一-龥]", node.surface):
-                    words_list.append(base)
+                if pos == "名詞":
+                    word = token.surface
+                else:
+                    word = token.feature.lemma or token.surface
 
-        node = node.next
+                words_list.append(word)
 
     return words_list
