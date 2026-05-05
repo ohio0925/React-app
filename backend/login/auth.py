d870abea90b3361e593b89346d63c349e3d887a1
ohio0925 <sinteito@gmail.com>
2026-05-05 23:33:58 +0900

    bcryptのみに修正

--- backend/login/auth.py
+++ backend/login/auth.py
@@ -10,12 +10,12 @@ ALGORITHM = "HS256"
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str):
-    digest = hashlib.sha256(password.encode()).hexdigest()
-    return pwd_context.hash(digest)
+    # digest = hashlib.sha256(password.encode()).hexdigest()
+    return pwd_context.hash(password)
 
 def verify_password(plain, hashed):
-    digest = hashlib.sha256(plain.encode()).hexdigest()
-    return pwd_context.verify(digest, hashed)
+    # digest = hashlib.sha256(plain.encode()).hexdigest()
+    return pwd_context.verify(plain, hashed)
 
 def create_token(user_id: str):
     payload = {
